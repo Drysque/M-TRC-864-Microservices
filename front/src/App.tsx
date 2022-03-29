0c7bb6be4b90e02364ce0fccf6eb8eb511f206f5
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2022-03-29 13:47:42 +0200

    Add theme

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { Routes } from './Routing';
+import theme from './theme';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
---
+++ front/src/theme/index.ts
@@ -0,0 +1,5 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import colors from './colors';
+
+export default extendTheme({ colors });
